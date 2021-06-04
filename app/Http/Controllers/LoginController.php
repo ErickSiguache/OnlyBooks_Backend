29aef345c05d82ba75582c8dac6ce4d79fdcf05e
Erick Manuel Aguilar Siguache <65965338+ErickSiguache@users.noreply.github.com>
2021-06-03 21:41:48 -0600

    Ruta para implementacion de Api Login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
+use Illuminate\Support\Str; //Esto es para generar un string aleatorio como token
 use App\Usuario;
 
 class LoginController extends Controller
@@ -36,18 +36,25 @@ class LoginController extends Controller
      */
     public function store(Request $request)
     {
+        $response = "";
+        //Sirve para extraer los datos de la peticion (Enviados por el front)
         $username = $request -> input('username');
         $password = $request -> input('password');
-        $usuario = new Usuario();
-        $usuarios = Usuarios::where('username', $username, 'password', $password ) ->get();
-        if($usuarios -> isEmpty()){
+        //Con esto se buscar el Usuario en la Base de datos
+        $usuario = Usuario::where('username', $username)
+                        ->where('password', $password)
+                        ->get();
+        if($usuario -> isEmpty()){
             $respu = 'Usuario no existe';
-        }elseif($usuarios -> isNotEmpty()){
+        }elseif($usuario -> isNotEmpty()){
             $token = Str::random(32);
-            $token2 = ['token' => $token];
-            $usuario2 = Usuarios::where('username', $username, 'password', $password) 
-                ->update($token2, ['upsert' => true]);
-                $respu = $token;
+            $token2 = [
+                'token' => $token
+            ];
+            $usuario2 = Usuario::where('username', $username)
+                            ->where('password', $password)
+                            ->update($token2, ['upsert' => true]);
+            $respu = $token;
         }
         return response() -> json($respu);
     }
@@ -83,9 +90,12 @@ class LoginController extends Controller
      */
     public function update(Request $request, $token)
     {
-        $token2 = ['token' => 'sinsesion'];
-        $usuario2 = Usuarios::where('token', $token) 
+        $token2 = [
+            'token' => 'sinsesion'
+        ];
+        $usuario2 = Usuario::where('token', $token) 
                 ->update($token2, ['upsert' => true]);
+        $rerpu = $token;
         return response() -> json(['message' => 'Sesion Cerrada']);
     }
 
--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 Route::resource('libro', 'LibroController');
 Route::resource('usuario', 'UsuarioController');
-Route::resource('user', 'AuthController');
-
+Route::resource('login', 'LoginController');
