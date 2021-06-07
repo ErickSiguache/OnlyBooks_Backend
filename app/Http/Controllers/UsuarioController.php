622ee4954a6ae5897c25911fced36dd21ebb483b
Erick Manuel Aguilar Siguache <65965338+ErickSiguache@users.noreply.github.com>
2021-06-07 00:04:22 -0600

    Agregando los tipos de usuario

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 use App\Usuario;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str; //Esto es para generar un string aleatorio como token
